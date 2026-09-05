63975120010ded91b06a86362691a241ea4266d6
damiangorosito <walter.gorosito@bue.edu.ar>
2026-09-05 20:46:54 -0300

    Update index.js
    Agregue app turnos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const profesionalesRouter = require("./routes/profesionales.route");
 const clientesRouter = require("./routes/clientes.route");
-
+const turnosRouter = require("./routes/turnos.route");
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 
 app.use('/profesionales', profesionalesRouter);
 app.use('/clientes', clientesRouter);
-
+app.use('/turnos', turnosRouter);
 // Puerto de escucha
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000");
